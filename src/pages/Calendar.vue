53d87e0ef7754b400dde627aa2cd08421c710070
seansljh <35062428+seansljh@users.noreply.github.com>
2020-03-16 23:00:37 +0800

    Update Calendar.Vue

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="content">
-    <div class="md-layout">
-    
-    </div>
-  </div>
+  <v-app id="dayspan" v-cloak>
+    <ds-calendar-app :calendar="calendar"></ds-calendar-app>
+  </v-app>
 </template>
 
 <script>
-
+import { Calendar } from 'dayspan';
 
 export default {
-
-  data() {
-    return {
-
-    };
-  }
-};
+  name: 'App',
+  data: () => ({
+    calendar: Calendar.months()
+  })
+}
 </script>
+
+<style>
+body, html, #app, #dayspan {
+  font-family: Roboto, sans-serif;
+  width: 100%;
+  height: 100%;
+}
+</style>
