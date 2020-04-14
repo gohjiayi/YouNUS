40992636c147ffab8635b600fc5c89daea6a3236
Jeremy Ng <50653562+jialormeee@users.noreply.github.com>
2020-04-14 11:49:10 +0800

    Update stickyNote.vue

--- src/components/stickyNote.vue
+++ src/components/stickyNote.vue
@@ -29,3 +29,72 @@
         <b-modal v-model="modalShow">Are you sure you want to delete this note? You will not get back</b-modal>
     </div>
 </template>
+
+
+
+<style lang="scss">
+.file-drop {
+    border: 1px solid #ebebeb;
+    margin: 20px;
+    padding-top: 1px;
+}
+
+.customdropzone {
+  font-family: 'Arial', sans-serif;
+  letter-spacing: 0.2px;
+  transition: background-color .2s linear;
+  // height: 200px;
+  padding: 0;
+  .dz-message{
+      margin: 5em 0 !important;
+  }
+
+  .dz-preview {
+    width: 160px;
+    display: inline-block;
+
+    .dz-image {
+      width: 80px;
+      height: 80px;
+      margin-left: 40px;
+      margin-bottom: 10px;
+
+      div {
+        width: inherit;
+        height: inherit;
+        border-radius: 50%;
+        background-size: contain;
+      }
+
+      img {
+        width: 100%;
+      }
+    }
+
+    .dz-details {
+      color: white;
+      transition: opacity .2s linear;
+      text-align: center;
+    }
+  }
+
+  .dz-success-mark, .dz-error-mark, .dz-remove {
+    display: none;
+  }
+}
+
+.slicky-note-list {
+    color: #212529;
+    margin: 15px;
+    cursor: pointer;
+}
+
+.remove-sticky {
+    margin-bottom: 10px;
+    span {
+        background: #ccc;
+        padding: 6px 10px;
+        border-radius: 50%;
+    }
+}
+</style>
